779d1cfb94ecf0005de7b65d3474298bdc0fe3c7
deddunft <99079667+deddunft@users.noreply.github.com>
2023-02-25 18:24:39 +0100

    opll

--- api/mapping.ts
+++ api/mapping.ts
@@ -96,6 +96,6 @@ export const stakePoolsWithHostnames: {
   
   {
     stakePoolAddress: 'BWTwtx9VNjo3aNwmMzb34JgePDFeFiNotczoQ4PKEuDk',
-    hostname: 'stake.lonfo.net',
+    hostname: 'https://lonfostake.netlify.app/',
   }
 ]
